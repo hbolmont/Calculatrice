2962fc2e1586e89123f1a33302d2df29807a15b8
hbolmont <91721158+hbolmont@users.noreply.github.com>
2023-03-20 10:46:47 +0100

    Création de tout les boutons

--- fenetre.cpp
+++ fenetre.cpp
@@ -4,6 +4,60 @@
 Fenetre::Fenetre(QWidget *parent) : QWidget(parent)
 {
     setFixedSize(600, 800);
-    m_bouton = new QPushButton("Boutton", this);
-    m_bouton->setGeometry(10, 10, 170, 30);
+    m_boutonegal = new QPushButton("=", this);
+    m_boutonegal->setGeometry(530, 700, 60, 60);
+
+    m_boutonmoins = new QPushButton("-", this);
+    m_boutonmoins->setGeometry(530, 630, 60, 60);
+
+    m_boutonfois = new QPushButton("x", this);
+    m_boutonfois->setGeometry(530, 560, 60, 60);
+
+    m_boutondivise = new QPushButton("/", this);
+    m_boutondivise->setGeometry(530, 490, 60, 60);
+
+    m_boutonplus = new QPushButton("+", this);
+    m_boutonplus->setGeometry(530, 420, 60, 60);
+
+    m_boutonjsp = new QPushButton("jsp", this);
+    m_boutonjsp->setGeometry(460, 700, 60, 60);
+
+    m_boutonvirgule = new QPushButton(".", this);
+    m_boutonvirgule->setGeometry(390, 700, 60, 60);
+
+    m_bouton0 = new QPushButton("0", this);
+    m_bouton0->setGeometry(320, 700, 60, 60);
+
+    m_bouton3= new QPushButton("3", this);
+    m_bouton3->setGeometry(460, 630, 60, 60);
+
+    m_bouton2 = new QPushButton("2", this);
+    m_bouton2->setGeometry(390, 630, 60, 60);
+
+    m_bouton1 = new QPushButton("1", this);
+    m_bouton1->setGeometry(320, 630, 60, 60);
+
+    m_bouton6 = new QPushButton("6", this);
+    m_bouton6->setGeometry(460, 560, 60, 60);
+
+    m_bouton5 = new QPushButton("5", this);
+    m_bouton5->setGeometry(390, 560, 60, 60);
+
+    m_bouton4 = new QPushButton("4", this);
+    m_bouton4->setGeometry(320, 560, 60, 60);
+
+    m_bouton9 = new QPushButton("9", this);
+    m_bouton9->setGeometry(460, 490, 60, 60);
+
+    m_bouton8 = new QPushButton("8", this);
+    m_bouton8->setGeometry(390, 490, 60, 60);
+
+    m_bouton7 = new QPushButton("7", this);
+    m_bouton7->setGeometry(320, 490, 60, 60);
+
+    m_boutonclear = new QPushButton("Clear", this);
+    m_boutonclear->setGeometry(320, 420, 130, 60);
+
+    m_boutoneffacer= new QPushButton("Effacer", this);
+    m_boutoneffacer->setGeometry(460, 420, 60, 60);
 }
--- fenetre.hpp
+++ fenetre.hpp
@@ -8,6 +8,24 @@ class Fenetre : public QWidget {
     public :
         Fenetre(QWidget * parent = nullptr);
     private :
-        QPushButton * m_bouton;
+        QPushButton * m_boutonplus;
+        QPushButton * m_boutonmoins;
+        QPushButton * m_boutondivise;
+        QPushButton * m_boutonfois;
+        QPushButton * m_boutonegal;
+        QPushButton * m_bouton0;
+        QPushButton * m_boutonvirgule;
+        QPushButton * m_boutonjsp;
+        QPushButton * m_bouton3;
+        QPushButton * m_bouton2;
+        QPushButton * m_bouton1;
+        QPushButton * m_bouton6;
+        QPushButton * m_bouton5;
+        QPushButton * m_bouton4;
+        QPushButton * m_bouton9;
+        QPushButton * m_bouton8;
+        QPushButton * m_bouton7;
+        QPushButton * m_boutonclear;
+        QPushButton * m_boutoneffacer;
 };
 #endif
